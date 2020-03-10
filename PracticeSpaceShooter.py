63fa4050546c2e98c6851409beb6c64aa2a0e457
JackBragg <Jackrocker418@gmail.com>
2020-03-09 18:08:35 -0700

    progress on VoE

--- PracticeSpaceShooter.py
+++ PracticeSpaceShooter.py
@@ -1,7 +1,6 @@
 '''
 A space based shooter based on the 
-Asteroid smasher tutorial for PyArcade
-TODO: 
+Asteroid smasher tutorial for PyArcade 
 
 '''
 
@@ -29,7 +28,6 @@ class TurningSprite(arcade.Sprite):
     ''' 
     Sprite that sets its angle to the direction it is facing,
     used to shoot projectiles
-    TODO: new class to cover tracking weapons like missles
     '''
     def update(self):
         super().update()
@@ -53,7 +51,8 @@ class ShipSprite(arcade.Sprite):
         self.thrust = 0
         self.speed = 0
         self.max_speed = 4
-        self.drag = 0.05
+        # self.drag = 0.05
+        self.drag = 0
         self.respawning = 0
 
         # mark that we are respawning
@@ -188,8 +187,10 @@ class MyGame(arcade.Window):
         self.asteroid_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
 
-        # set up the player
+        # Set up the player
+        self.score = 0
         self.player_sprite = None
+        self.lives = 3
 
         # sounds
         self.laser_sound = arcade.load_sound(":resources:sounds/hurt5.wav")
@@ -202,6 +203,118 @@ class MyGame(arcade.Window):
         '''
         sets up game and initalizes the variables
         '''
+        self.frame_count = 0
+        self.game_over = False
+
+        # Sprite lists
+        self.all_sprites_list = arcade.SpriteList()
+        self.asteroid_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+        self.ship_life_list = arcade.SpriteList()
+
+        # Set up the player
+        self.score = 0
+        self.player_sprite = ShipSprite(":resources:images/space_shooter/playerShip1_orange.png", SCALE)
+        self.all_sprites_list.append(self.player_sprite)
+        self.lives = 3
+
+        # Make asteroids
+        image_list = (":resources:images/space_shooter/meteorGrey_big1.png",
+                      ":resources:images/space_shooter/meteorGrey_big2.png",
+                      ":resources:images/space_shooter/meteorGrey_big3.png",
+                      ":resources:images/space_shooter/meteorGrey_big4.png")
+        
+        for i in range(STARTING_ENEMY_COUNT):
+            image_no = random.randrange(4)
+            enemy_sprite = AsteroidSprite(image_list[image_no], SCALE)
+            enemy_sprite.guid = "Asteroid"
+
+            # spawn asteroid
+            enemy_sprite.center_x = random.randrange(LEFT_LIMIT, RIGHT_LIMIT)
+            enemy_sprite.center_y = random.randrange(BOTTOM_LIMIT, TOP_LIMIT)
+
+            # randomizes asteroid speed
+            enemy_sprite.change_x = random.random() * 2 - 1
+            enemy_sprite.change_y = random.random() * 2 - 1
+
+            # sets asteroid off in random direction
+            enemy_sprite.change_angle = (random.random() - 0.5) * 2
+            enemy_sprite.size = 4
+            self.all_sprites_list.append(enemy_sprite)
+            self.asteroid_list.append(enemy_sprite)
+
+    def on_draw(self):
+        '''
+        render the screen
+        '''
+
+        # required before we start drawing
+        arcade.start_render()
+
+        # draw all the sprites
+        self.all_sprites_list.draw()
+
+    def on_key_press(self, symbol, modifiers):
+        '''
+        called on key press
+        controls
+        '''
+        # shoot if the player presses key and not respawning
+        # TODO: fire two projectiles at a time (spawn offset)
+        if not self.player_sprite.respawning and symbol == arcade.key.SPACE:
+            bullet_sprite = BulletSprite(":resources:images/space_shooter/laserBlue01.png", SCALE)
+            bullet_sprite.guid = "Bullet"
+
+            # sets bullet in direction player is facing (player angle)
+            # sets bullet speed
+            # TODO: create seperate class for different weapons with different speeds / range
+            bullet_speed = 13
+            bullet_sprite.change_x = \
+                -math.sin(math.radians(self.player_sprite.angle)) * bullet_speed
+            bullet_sprite.change_y = \
+                math.cos(math.radians(self.player_sprite.angle)) * bullet_speed
+
+            # spawns bullet at player center
+            # TODO: offset projectile spawn from weapon locations on player
+            bullet_sprite.center_x = self.player_sprite.center_x
+            bullet_sprite.center_y = self.player_sprite.center_y
+            bullet_sprite.update()
+
+            # adds new bullet to all/list and bullet list
+            self.all_sprites_list.append(bullet_sprite)
+            self.bullet_list.append(bullet_sprite)
+
+            # TODO: derive sound from new weapon class
+            arcade.play_sound(self.laser_sound)
+
+        if symbol == arcade.key.A:
+            self.player_sprite.change_angle = 3
+        elif symbol == arcade.key.D:
+            self.player_sprite.change_angle = -3
+        # TODO: Pull thrust from ship class
+        elif symbol == arcade.key.W:
+            self.player_sprite.thrust = 0.15
+        # TODO: Pull reverse thrust from ship class
+        elif symbol == arcade.key.S:
+            self.player_sprite.thrust = -0.1
+
+    def on_key_release(self, symbol, modifiers):
+        '''
+        called when key is released
+        '''
+        if symbol == arcade.key.A:
+            self.player_sprite.change_angle = 0
+        elif symbol == arcade.key.D:
+            self.player_sprite.change_angle = 0
+        # TODO: Pull thrust from ship class
+        elif symbol == arcade.key.W:
+            self.player_sprite.thrust = 0
+        # TODO: Pull reverse thrust from ship class
+        elif symbol == arcade.key.S:
+            self.player_sprite.thrust = 0
+        
+
+
 
 
 
